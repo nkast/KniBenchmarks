e15e9531af3a6a96112b5be82901e54a60f99107
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 20:16:41 +0200

    templatestudio

---
+++ src/DrawBenchmarks/winui3/Benchmarks/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Benchmarks.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ src/DrawBenchmarks/winui3/Benchmarks/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Benchmarks.Helpers;
+
+using Windows.UI.ViewManagement;
+
+namespace Benchmarks;
+
+public sealed partial class MainWindow : WindowEx
+{
+    private Microsoft.UI.Dispatching.DispatcherQueue dispatcherQueue;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+
+        // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
+        dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+        
+    }
+}
