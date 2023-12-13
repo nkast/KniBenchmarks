2525dd9d18656d9a40c8e29db709bb2f80e09c7d
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 19:04:42 +0200

    u

--- src/DrawBenchmarks/Benchmarks.Kni.WinUI/MauiProgram.cs
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 
 namespace Benchmarks
 {
@@ -9,11 +11,12 @@ namespace Benchmarks
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                //.ConfigureFonts(fonts =>
+                //{
+                //    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                //    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                //})
+                ;
 
 #if DEBUG
     		builder.Logging.AddDebug();
