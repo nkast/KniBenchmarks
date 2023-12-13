55c5dae85634a24d950ed6c6ec6be97379e02316
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 18:22:46 +0200

    gg

--- src/DrawBenchmarks/Benchmarks.Kni.WinUI/App.xaml.cs
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/App.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Benchmarks
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Benchmarks
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            //
+            //var swapChainPanel = new Microsoft.UI.Xaml.Controls.SwapChainPanel();
+            Microsoft.UI
+            var swapChainPanel = new SwapChainPanel();
 
             MainPage = new AppShell();
         }
