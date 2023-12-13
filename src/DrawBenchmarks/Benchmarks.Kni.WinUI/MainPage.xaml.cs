5356b6b1d588f30121f1b9eb363e9741643016fa
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 18:22:16 +0200

    ff

--- src/DrawBenchmarks/Benchmarks.Kni.WinUI/MainPage.xaml.cs
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/MainPage.xaml.cs
@@ -5,6 +5,10 @@
         public MainPage()
         {
             InitializeComponent();
+
+            // Create the game.
+            string launchArguments = String.Empty;
+            //_game = MonoGame.Framework.XamlGame<BenchmarksGame>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
         }
     }
 
