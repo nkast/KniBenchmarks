bc293f43100185544ca8d390d6a74ca049c6573b
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 17:15:46 +0200

    MAUI project

---
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Benchmarks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Benchmarks
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
