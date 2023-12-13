ae466facd763dde0c474c4b55f38c8b01ac330b2
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-13 21:25:57 +0200

    test

--- src/DrawBenchmarks/Benchmarks.Kni.WinUI/Program.cs
+++ src/DrawBenchmarks/Benchmarks.Kni.WinUI/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace Benchmarks
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Microsoft.UI.Xaml.Application.Start((p) => new App());
-        }
-    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Microsoft.UI.Xaml.Application.Start((p) => new App());
+    //    }
+    //}
 }
