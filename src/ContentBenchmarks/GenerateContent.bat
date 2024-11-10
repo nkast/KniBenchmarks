e98fb88421064b2e6bdbf7501968a14d378f78bc
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-11 01:59:19 +0200

    added BenchmarksModelsContent

--- src/ContentBenchmarks/GenerateContent.bat
+++ src/ContentBenchmarks/GenerateContent.bat
@@ -2,3 +2,4 @@ for /L %%A in (1001,1,1064) do copy "BenchmarksContent\BenchmarksSounds\Sound.wa
 for /L %%A in (1001,1,1064) do copy "BenchmarksContent\BenchmarksFonts\Font.spritefont" "BenchmarksContent\BenchmarksFonts\Font%%A.spritefont"
 for /L %%A in (1001,1,1064) do copy "BenchmarksContent\BenchmarksTextures\Tx.png" "BenchmarksContent\BenchmarksTextures\Tx%%A.png"
 for /L %%A in (1001,1,1064) do copy "BenchmarksContent\BenchmarksEffects\Effect.fx" "BenchmarksContent\BenchmarksEffects\Effect%%A.fx"
+for /L %%A in (1001,1,1064) do copy "BenchmarksContent\BenchmarksModels\Model.fbx" "BenchmarksContent\BenchmarksModels\Model%%A.fbx"
